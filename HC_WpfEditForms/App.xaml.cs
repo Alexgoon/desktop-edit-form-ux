40ab59b5a5927286fa76ac2c5b4414c19b99a19d
Alexgoon <alexander.russkov@devexpreess.com>
2025-04-01 22:19:16 +0400

    initial commit

---
+++ HC_WpfEditForms/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Azure.AI.OpenAI;
+using DevExpress.AIIntegration;
+using DevExpress.Xpf.Core;
+using Microsoft.Extensions.AI;
+using System;
+using System.Runtime;
+using System.Windows;
+
+namespace HC_WpfEditForms;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    static App()
+    {
+        CompatibilitySettings.UseLightweightThemes = true;
+        //#if (shell == none)
+        ApplicationThemeHelper.Preload(PreloadCategories.Core);
+        //#endif
+        ApplicationThemeHelper.ApplicationThemeName = LightweightTheme.Win11Light.Name;
+
+        //Enable Multi Core JIT to improve startup performance
+        ProfileOptimization.SetProfileRoot(AppContext.BaseDirectory);
+        ProfileOptimization.StartProfile("Startup.Profile");
+
+        ClearAutomationEventsHelper.IsEnabled = false;
+        
+        IChatClient asChatClient = new AzureOpenAIClient(new Uri("[YOUR ENDPOINT]"),
+           new System.ClientModel.ApiKeyCredential("[KEY]"))
+           .AsChatClient("[MDOEL NAME]");
+        AIExtensionsContainerDesktop.Default.RegisterChatClient(asChatClient);
+    }
+
+
+}
+
---
+++ HC_WpfEditForms/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using DevExpress.Xpf.Core;
+using DevExpress.XtraRichEdit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Mvvm.CodeGenerators;
+using DevExpress.Mvvm.DataAnnotations;
+
+namespace HC_WpfEditForms
+{
+    //Properties and commands are generated automatically for the following class when compiling:
+    //Refer to the following help topic for details: https://docs.devexpress.com/WPF/402989/mvvm-framework/viewmodels/compile-time-generated-viewmodels
+    [GenerateViewModel]
+    public partial class MainViewModel
+    {
+        [GenerateProperty]
+        IEnumerable<string> countries = [
+            "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria",
+            "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan",
+            "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia",
+            "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo (Congo-Brazzaville)", "Costa Rica",
+            "Croatia", "Cuba", "Cyprus", "Czechia", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador",
+            "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France",
+            "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau",
+            "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland",
+            "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan",
+            "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar",
+            "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia",
+            "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal",
+            "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan",
+            "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar",
+            "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia",
+            "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa",
+            "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Tajikistan",
+            "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu",
+            "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela",
+            "Vietnam", "Yemen", "Zambia", "Zimbabwe"
+        ];
+
+
+        [GenerateCommand]
+        async Task Save()
+        {
+            await Task.Delay(700);
+            ThemedMessageBox.Show("Asynchronous save action");
+        }
+    }
+
+    public enum PersonPrefix
+    {
+        Dr,
+        Mr,
+        Ms,
+        Miss,
+        Mrs
+    }
+}
